4f2ad80c24b7e197c16f782deab767d8feb947cf
lucaspausin <lucaspausin@gmail.com>
2024-06-07 04:10:31 -0300

    Añadiendo integración de robots.txt para mejorar el SEO del sitio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
+import robotsTxt from "astro-robots-txt";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+	site: "https://lucaspausin.up.railway.app/",
+
+	integrations: [tailwind(), react(), robotsTxt()],
 });
